0752efd03e14ff60629a8f5cf36a6bbc0458e40b
Javier Sánchez <js.alarcon09@uma.es>
2025-01-06 01:42:32 +0100

    Web-Server, DataServer y Cliente html

---
+++ src/dataserver.py
@@ -0,0 +1,223 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, CheckConstraint
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from datetime import datetime, timezone, timedelta
+import zmq
+import json
+
+# Configuración de SQLAlchemy
+Base = declarative_base()
+
+# Málaga tiene UTC +1 en horario estándar (invierno)
+malaga_timezone = timezone(timedelta(hours=1))  # UTC +1
+
+NODE_NAME = "nodo1"
+
+# O para el horario de verano (UTC +2)
+# malaga_timezone = timezone(timedelta(hours=2))  # UTC +2
+
+class Inventario(Base):
+    __tablename__ = 'inventario'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    objeto = Column(String(70), nullable=False)
+    cantidad = Column(Integer, nullable=True, default=0)
+    reservados = Column(Integer, nullable=True, default=0)
+    CheckConstraint(
+        'cantidad IS NOT NULL OR reservados IS NOT NULL',
+        name='check_atributos_no_nulos'
+    )
+
+
+class Movimientos(Base):
+    __tablename__ = 'movimientos'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    objeto = Column(String(70), ForeignKey('inventario.id'), nullable=False)
+    tipo = Column(String(50), nullable=False)  # "entrada", "salida", "reserva", "cancelacion reserva", "sacar reserva" 
+    cantidad = Column(Integer, nullable=True, default=0)
+    reservados = Column(Integer, nullable=True, default=0)
+    fecha = Column(DateTime, default=datetime.now(malaga_timezone), nullable=False)
+    CheckConstraint(
+        'cantidad IS NOT NULL OR reservados IS NOT NULL',
+        name='check_atributos_no_nulos'
+    )
+    # Relación opcional para acceder al objeto Inventario asociado
+    inventario = relationship('Inventario', backref='movimientos')
+
+
+engine = create_engine(f'sqlite:///almacen.db')
+
+
+# Función para cambiar la base de datos y crear Session
+def cambiar_base_datos(nueva_bd):
+    global engine, Session
+    engine = create_engine(f'sqlite:///{nueva_bd}')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    print(f"Base de datos cambiada a {nueva_bd}")
+
+
+# Inicializar la sesión con la base de datos por defecto
+cambiar_base_datos("almacen.db")
+
+def manejar_mensaje(mensaje_json):
+    session = Session()
+    try:
+        mensaje = json.loads(mensaje_json)  # Decodificar mensaje JSON
+        accion = mensaje.get("accion")
+        if not accion:
+            return json.dumps({"status": "error", "message": "Acción no especificada"})
+
+        nombre = mensaje.get("nombre")
+        cantidad = int(mensaje.get("cantidad"))
+        if not nombre or cantidad is None:
+            return json.dumps({"status": "error", "message": "Faltan datos: nombre o cantidad"})
+        item = session.query(Inventario).filter_by(objeto=nombre).first()
+
+        if not item:
+            return json.dumps({"status": "error", "message": f"Objeto {nombre} no encontrado en el inventario"})
+        if accion == "annadir":
+            if item:
+                item.cantidad += cantidad
+            else:
+                item = Inventario(objeto=nombre, cantidad=cantidad)
+                session.add(item)
+
+            # Registrar movimiento
+            movimiento = Movimientos(
+                objeto=nombre,
+                tipo="entrada",
+                cantidad=cantidad,
+                reservados=item.reservados,
+                fecha=datetime.now(malaga_timezone)
+            )
+            session.add(movimiento)
+            session.commit()
+            return json.dumps({"status": "success", "nombre": nombre, "cantidad":item.cantidad,"reservados":item.reservados,"mensaje": f"Objeto {nombre} annadido con cantidad {cantidad}"})
+
+        elif accion == "sacar":
+            if item.cantidad < cantidad:
+                return json.dumps({"status": "error", "message": f"Cantidad insuficiente de {nombre} en el inventario"})
+
+            movimiento = Movimientos(
+                objeto=nombre,
+                tipo="salida",
+                cantidad=cantidad,
+                reservados=item.reservados,
+                fecha=datetime.now(malaga_timezone)
+            )
+            item.cantidad -= cantidad
+            if item.cantidad == 0 and item.reservados == 0:
+                session.delete(item)  # Eliminar el objeto si la cantidad llega a 0
+
+            session.add(movimiento)
+            session.commit()
+
+            return json.dumps({"status": "success", "nombre": nombre, "cantidad":item.cantidad,"reservados":item.reservados,"mensaje": f"Objeto {nombre} sacado con cantidad {cantidad}"})
+
+        if accion == "reservar":
+            if item.cantidad < cantidad:
+                return json.dumps({"status": "error", "message": f"Cantidad insuficiente de {nombre} para reservar"})
+
+            item.cantidad -= cantidad
+            item.reservados += cantidad
+
+            movimiento = Movimientos(
+                objeto=nombre,
+                tipo="reserva",
+                cantidad=item.cantidad,
+                reservados=item.reservados,
+                fecha=datetime.now(malaga_timezone)
+            )
+            session.add(movimiento)
+            session.commit()
+
+            return json.dumps({"status": "success", "nombre": nombre, "cantidad":item.cantidad,"reservados":item.reservados,"mensaje": f"Objeto {nombre} reservado con cantidad {cantidad}"})
+
+        elif accion == "cancelar_reserva":
+            if item.reservados < cantidad:
+                return json.dumps({"status": "error", "message": f"Cantidad insuficiente de {nombre} reservada para cancelar"})
+
+            item.cantidad += cantidad
+            item.reservados -= cantidad
+
+            movimiento = Movimientos(
+                objeto=nombre,
+                tipo="cancelacion reserva",
+                cantidad=item.cantidad,
+                reservados=item.reservados,
+                fecha=datetime.now(malaga_timezone)
+            )
+            session.add(movimiento)
+            session.commit()
+
+            return json.dumps({"status": "success", "nombre": nombre, "cantidad":item.cantidad,"reservados":item.reservados,"mensaje": f"Objeto {nombre} reserva cancelada con cantidad {cantidad}"})
+
+        elif accion == "sacar_reserva":
+            if item.reservados < cantidad:
+                return json.dumps({"status": "error", "message": f"Cantidad insuficiente de {nombre} reservada para retirar"})
+
+            item.reservados -= cantidad
+
+            movimiento = Movimientos(
+                objeto=nombre,
+                tipo="sacar reserva",
+                cantidad=item.cantidad,
+                reservados=item.reservados,
+                fecha=datetime.now(malaga_timezone)
+            )
+            session.add(movimiento)
+            session.commit()
+
+            return json.dumps({"status": "success", "nombre": nombre, "cantidad":item.cantidad,"reservados":item.reservados,"mensaje": f"Objeto {nombre} reserva sacada con cantidad {cantidad}"})
+        elif accion == "ver":
+            contenido = session.query(Inventario).all()
+            resultado = [
+                {"nombre": item.objeto, "cantidad": item.cantidad, "reservados": item.reservados}
+                for item in contenido
+            ]
+            return json.dumps({"status": "success", "inventario": resultado})
+
+        elif accion == "get_item_quantity":
+
+            return json.dumps({"status": "success", "data": {"nombre": nombre, "cantidad": item.cantidad}})
+
+        elif accion == "get_node_name":
+            return json.dumps({"status": "success", "data": {"node_name": NODE_NAME}})
+
+        elif accion == "remove_db":
+            cambiar_base_datos(f"{nombre}")
+            return json.dumps({"status": "success", "data": {"nombre": nombre, "mensaje": f"Nueva base de datos {nombre}"}})
+
+        else:
+            return json.dumps({"status": "error", "message": "Acción no reconocida"})
+
+    except Exception as e:
+        session.rollback()
+        return json.dumps({"status": "error", "message": f"Error al manejar el mensaje: {e}"})
+
+    finally:
+        session.close()
+
+def servidor():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)  # REP para recibir peticiones y enviar respuestas
+    socket.bind("tcp://*:5555")  # Escuchar en el puerto 5555
+
+    print("Servidor iniciado y esperando mensajes...")
+
+    while True:
+        try:
+            mensaje = socket.recv_string()  # Recibir mensaje como string
+            print(f"Mensaje recibido: {mensaje}")
+
+            respuesta = manejar_mensaje(mensaje)  # Manejar el mensaje
+
+            socket.send_string(respuesta)  # Enviar respuesta
+
+        except Exception as e:
+            print(f"Error al procesar el mensaje: {e}")
+
+
+if __name__ == "__main__":
+    servidor()
---
+++ src/web-server.py
@@ -0,0 +1,110 @@
+import zmq
+import zmq.error
+import json
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__,template_folder= '../templates')
+
+def enviar_mensaje(mensaje_dict):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)  # REQ para enviar peticiones
+    socket.connect("tcp://localhost:5555")  # Conecta al servidor en el puerto 5555
+
+    # Establecer un timeout de 5 segundos para las respuestas
+    socket.setsockopt(zmq.RCVTIMEO, 5000)  # 5000 ms = 5 segundos
+    try:
+        mensaje_json = json.dumps(mensaje_dict, ensure_ascii=False)
+        print(f"Enviando: {mensaje_json}")
+        socket.send_json(mensaje_dict)
+
+        # Intentar recibir respuesta del servidor
+        respuesta_json = socket.recv_json()
+        respuesta=respuesta_json.get("mensaje","No hay mensaje en el JSON")
+        print(f"Respuesta recibida: {respuesta}")
+        #return json.loads(respuesta)  # Decodificar la respuesta JSON
+        return respuesta_json
+    except zmq.error.Again:
+        # Manejar el caso de timeout
+        print("No se recibió respuesta del servidor (timeout)")
+        return {"error": "No se recibió respuesta del servidor (timeout)"}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/inventario/annadir', methods=['POST'])
+def annadir_objeto():
+    datos = request.json
+    nombre = datos.get('nombre')
+    cantidad = datos.get('cantidad')
+    mensaje = {
+        "accion": "annadir",
+        "nombre": nombre,
+        "cantidad": cantidad
+    }
+    respuesta = enviar_mensaje(mensaje)
+    return jsonify(respuesta)
+
+@app.route('/inventario/sacar', methods=['POST'])
+def sacar_objeto():
+    datos = request.json
+    nombre = datos.get('nombre')
+    cantidad = datos.get('cantidad')
+    mensaje = {
+        "accion": "sacar",
+        "nombre": nombre,
+        "cantidad": cantidad
+    }
+    respuesta = enviar_mensaje(mensaje)
+    return jsonify(respuesta)
+
+@app.route('/inventario/reservar', methods=['POST'])
+def reservar():
+    datos = request.json
+    nombre = datos.get('nombre')
+    cantidad = datos.get('cantidad')
+    mensaje = {
+        "accion": "reservar",
+        "nombre": nombre,
+        "cantidad": cantidad
+    }
+    respuesta = enviar_mensaje(mensaje)
+    return jsonify(respuesta)
+
+@app.route('/inventario/sacar_reserva', methods=['POST'])
+def sacar_reserva():
+    datos = request.json
+    nombre = datos.get('nombre')
+    cantidad = datos.get('cantidad')
+    mensaje = {
+        "accion": "sacar_reserva",
+        "nombre": nombre,
+        "cantidad": cantidad
+    }
+    respuesta = enviar_mensaje(mensaje)
+    return jsonify(respuesta)
+
+@app.route('/inventario/cancelar_reserva', methods=['POST'])
+def cancelar_reserva():
+    datos = request.json
+    nombre = datos.get('nombre')
+    cantidad = datos.get('cantidad')
+    mensaje = {
+        "accion": "cancelar_reserva",
+        "nombre": nombre,
+        "cantidad": cantidad
+    }
+    respuesta = enviar_mensaje(mensaje)
+    return jsonify(respuesta)
+
+@app.route('/inventario/ver', methods=['GET'])
+def ver_inventario():
+    mensaje = {
+        "accion": "ver"
+    }
+    respuesta = enviar_mensaje(mensaje)
+    return jsonify(respuesta)
+
+if __name__ == "__main__":
+    puerto = int(input("Introduce un puerto para el nodo: "))
+    app.run(port=puerto)
